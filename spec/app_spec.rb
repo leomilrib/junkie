07236d318b745154bf62823c3eee9e5f743d5588
leomilrib <leo@leomilrib.com>
2015-02-01 23:23:24 -0200

    Add pending specs for Octokit::Client

---
+++ spec/app_spec.rb
@@ -0,0 +1,82 @@
+describe 'app' do
+  let(:octo_client) {
+    instance_double('Octokit::Client',
+      issue_comments: [
+        {
+          html_url:'https://github.com/org/repo/pull/666#issuecomment-666666',
+          id:666666,
+          user:{
+            login:'user',
+            id:666667,
+            type:'User'
+          },
+         body:':+1: '
+        },
+        {
+          html_url:'https://github.com/org/repo/pull/666#issuecomment-666666',
+          id:666666,
+          user:{
+            login:'another_user',
+            id:666668,
+            type:'User',
+          },
+          body:':+1: '
+        }
+      ],
+      pull_comments: [
+        {
+          url:'https://api.github.com/repos/org/repo/pulls/comments/666666',
+          id:666666,
+          diff_hunk:'<<code>>',
+          position:1,
+            user:{
+            login:'user',
+            id:666666,
+            type:'User'
+          },
+          body:'yay! comments!',
+          html_url:'https://github.com/org/repo/pull/666#discussion_r666666',
+          pull_request_url:'https://api.github.com/repos/org/repo/pulls/666',
+        },
+        {
+          url:'https://api.github.com/repos/org/repo/pulls/comments/666666',
+          id:666667,
+          diff_hunk:'<<code>>',
+          position:1,
+          user:{
+            login:'another_user',
+            id:666667,
+            type:'User'
+          },
+          body:':grin:',
+          html_url:'https://github.com/org/repo/pull/666#discussion_r666666',
+          pull_request_url:'https://api.github.com/repos/org/repo/pulls/666',
+        },
+        {
+          url:'https://api.github.com/repos/org/repo/pulls/comments/666666',
+          id:666668,
+          diff_hunk:'<<code>>',
+          position:1,
+            user:{
+            login:'and_another',
+            id:666668,
+            type:'User'
+          },
+          body:'and more comments!',
+          html_url:'https://github.com/org/repo/pull/666#discussion_r666666',
+          pull_request_url:'https://api.github.com/repos/org/repo/pulls/666',
+        }
+      ]
+    )
+  }
+
+  it 'returns true if pull can be merged' do
+    # octo_client.issue_comments('org/repo',666)
+    pending 'define method'
+  end
+
+  it 'returns true if pull have comments' do
+    # octo_client.pull_comments('org/repo',666)
+    pending 'define method'
+  end
+end
