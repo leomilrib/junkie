b2c9019717cf20bf151b37b1da0137b7c614f47a
leomilrib <leo@leomilrib.com>
2015-03-04 23:31:23 -0300

    Add faraday caching for octokit

--- app.rb
+++ app.rb
@@ -2,11 +2,19 @@ require 'sinatra'
 require 'sinatra/flash'
 require 'octokit'
 require 'httparty'
+require 'faraday-http-cache'
 require 'json'
 require './app_helpers'
 
 enable :sessions
 set :session_secret, (ENV["SESSION_SECRET"] || "this is session secret")
+
+stack = Faraday::RackBuilder.new do |builder|
+  builder.use Faraday::HttpCache
+  builder.use Octokit::Response::RaiseError
+  builder.adapter Faraday.default_adapter
+end
+Octokit.middleware = stack
 
 get '/' do
   if session[:user]
