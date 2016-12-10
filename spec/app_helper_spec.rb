c74f85c204b31aae5a7abb53e3742b10899b173e
leomilrib <leo@leomilrib.com>
2016-12-10 16:11:09 -0200

    No half truths...

--- spec/app_helper_spec.rb
+++ spec/app_helper_spec.rb
@@ -4,36 +4,36 @@ describe "app_helper" do
   it 'returns true if pull can be merged' do
     result = can_merge_it?(octo_client_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_truthy
+    expect(result).to eq(true)
   end
 
   it 'returns false if pull can not be merged' do
     result = can_merge_it?(octo_client_not_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_falsey
+    expect(result).to eq(false)
   end
 
   it "returns false if you didn't reviewed yet" do
     result = reviewed_it?(octo_client_not_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_falsey
+    expect(result).to eq(false)
   end
 
   it "returns true if you reviewed it" do
     result = reviewed_it?(octo_client_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_truthy
+    expect(result).to eq(true)
   end
 
   it "returns true if you commented it" do
     result = comments?(octo_client_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_truthy
+    expect(result).to eq(true)
   end
 
   it "returns false if you didn't commented it" do
     result = comments?(octo_client_not_ready.issue_timeline('org/repo',666))
 
-    expect(result).to be_falsey
+    expect(result).to eq(false)
   end
 end
